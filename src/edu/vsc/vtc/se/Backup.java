4c7d699f86a4c7c079d7b34dc821ebdf644a5943
Tom P <tompelchat@gmail.com>
2015-11-03 14:40:05 -0500

    testing

--- src/edu/vsc/vtc/se/Backup.java
+++ src/edu/vsc/vtc/se/Backup.java
@@ -5,3 +5,4 @@ public class Backup
 	//Delete this whole thing, its a lie, we will need to do this again fuck!!!!!
 }
 //this is not a test. this is a real comment. do not take it lightly
+// Testing out the cloning process. ~tp 
