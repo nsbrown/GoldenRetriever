faa0a84c66232b8e263621afa921833df8f30d58
Nathan Brown <nathanstormbrown@gmail.com>
2015-11-17 13:28:05 -0500

    hello, world

--- src/edu/vsc/vtc/se/Backup.java
+++ src/edu/vsc/vtc/se/Backup.java
@@ -2,17 +2,8 @@ package edu.vsc.vtc.se;
 
 public class Backup
 {
-	//Hey bitches
-	//Delete this whole thing, its a lie, we will need to do this again fuck!!!!!
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
 }
-//this is not a test. this is a real comment. do not take it lightly
-
-// Testing out the cloning process. ~tp 
-
-//Trying just a git push
-
-
-//Trying just a git push
-
-//poops poops poops
 
