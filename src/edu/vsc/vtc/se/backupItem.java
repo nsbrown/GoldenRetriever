c2423563078aaac32fb73498bff4511347eabef1
Earl Bombard <ewb09190@gmail.com>
2015-11-17 13:56:48 -0500

    added backupItem

---
+++ src/edu/vsc/vtc/se/backupItem.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package edu.vsc.vtc.se;
+
+import java.io.File;
+
+/**
+ * @author Frodo
+ *
+ */
+public class backupItem 
+{
+	String dir;
+	File fileName;
+	
+	backupItem(String directory, File fName)
+	{
+		dir = directory;
+		fileName = fName;
+	}
+	
+	void getFile()
+	{
+		
+	}
+	void setFile()
+	{
+		
+	}
+}
