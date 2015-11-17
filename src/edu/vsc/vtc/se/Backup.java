93110ac870cd9680469fa32a95e09b6a2b50d013
Earl Bombard <ewb09190@gmail.com>
2015-11-17 18:08:47 -0500

    updated

--- src/edu/vsc/vtc/se/Backup.java
+++ src/edu/vsc/vtc/se/Backup.java
@@ -7,25 +7,12 @@ public class Backup
 {
 	public static void main(String[] args)
 	{
-		String dir;
-		File filename;
+		
 	}
 	Backup()
 	{
 		
 	}
 	
-	//Hey bitches
-	//Delete this whole thing, its a lie, we will need to do this again fuck!!!!!
 }
-//this is not a test. this is a real comment. do not take it lightly
-
-// Testing out the cloning process. ~tp 
-
-//Trying just a git push
-
-
-//Trying just a git push
-
-//poops poops poops
 
