2e9787851c501117665563009415bfd810fba7b5
Earl Bombard <ewb09190@gmail.com>
2015-11-18 19:38:30 -0500

    updated backup & backupItem classes

--- src/edu/vsc/vtc/se/Backup.java
+++ src/edu/vsc/vtc/se/Backup.java
@@ -7,6 +7,12 @@ public class Backup
 {
 	 public static void main(String [] aArgs) throws IOException
 	  {
-		
+		 	File folder = new File("C:\\Test1");
+			File[] listofFiles = folder.listFiles();
+			String directory = "E:\\Test1\\";
+			
+			//backupItem newBackup = 
+			new backupItem(folder, listofFiles, directory);
+			
 	  }
 }
