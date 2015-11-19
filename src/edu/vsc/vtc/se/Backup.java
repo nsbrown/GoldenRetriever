ea1cfaf51505b20267c6cdb695a3b7514d86957f
Earl Bombard <ewb09190@gmail.com>
2015-11-19 13:09:42 -0500

    Updated backup class

--- src/edu/vsc/vtc/se/Backup.java
+++ src/edu/vsc/vtc/se/Backup.java
@@ -2,11 +2,34 @@ package edu.vsc.vtc.se;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Backup
 {
 	 public static void main(String [] aArgs) throws IOException
 	  {
+		 	/**
+		 	 * is going to take an array of file sources to send sources to backupItem class
+		 	 * So needs to loop through array. or could be a list which ever is easier
+		 	 */
+		 /**	int done = 0;
+		 	Scanner sourceReader = new Scanner(System.in);
+		 	String source;
+		 	//File[] arraySources;
+		 	List<File> listSource = new ArrayList<File>();
+		 	do
+		 	{
+		 		
+			 	System.out.println("choose the file path you are looking for");
+			 	source = sourceReader.next();
+			 	File newSource = new File(source);
+			 	listSource.add(newSource);
+			 	System.out.println("are you done?");
+		 	}while(!(source.equals("done")));
+		 	
+		 	*/
 		 	File folder = new File("C:\\Test1");
 			File[] listofFiles = folder.listFiles();
 			String directory = "E:\\Test1\\";
@@ -17,4 +40,11 @@ public class Backup
 			System.out.println("Time to Copy in seconds = " + ((System.nanoTime() - start))/1000000000);
 
 	  }
+	 private void listOfSources(List<File> sources)
+	 {
+		 for(int i = 0; i < sources.size(); i++ )
+		 {
+			 
+		 }
+	 }
 }
