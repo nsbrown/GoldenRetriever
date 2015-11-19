c61d7ddeb88b83cbd7607ba1d631b13c82b9008b
Earl Bombard <ewb09190@gmail.com>
2015-11-18 21:44:25 -0500

    took out commits

--- src/edu/vsc/vtc/se/Backup.java
+++ src/edu/vsc/vtc/se/Backup.java
@@ -12,7 +12,9 @@ public class Backup
 			String directory = "E:\\Test1\\";
 			
 			//backupItem newBackup = 
+			double start = System.nanoTime();
 			new backupItem(folder, listofFiles, directory);
-			
+			System.out.println("Time to Copy in seconds = " + ((System.nanoTime() - start))/1000000000);
+
 	  }
 }
