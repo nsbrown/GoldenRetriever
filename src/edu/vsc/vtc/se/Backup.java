0b9a05c46bdca4c9e42401261b0a0004b6d444a7
Earl Bombard <ewb09190@gmail.com>
2015-11-18 20:07:48 -0500

    updated master with the correct changed

--- src/edu/vsc/vtc/se/Backup.java
+++ src/edu/vsc/vtc/se/Backup.java
@@ -1,18 +1,18 @@
 package edu.vsc.vtc.se;
 
 import java.io.File;
-
+import java.io.IOException;
 
 public class Backup
 {
-	public static void main(String[] args)
-	{
-		
-	}
-	Backup()
-	{
-		
-	}
-	
+	 public static void main(String [] aArgs) throws IOException
+	  {
+		 	File folder = new File("C:\\Test1");
+			File[] listofFiles = folder.listFiles();
+			String directory = "E:\\Test1\\";
+			
+			//backupItem newBackup = 
+			new backupItem(folder, listofFiles, directory);
+			
+	  }
 }
-
